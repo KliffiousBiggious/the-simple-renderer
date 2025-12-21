6049693f3e472108a3b987ed8599fd76dfe5dd3c
KliffiousBiggious <inspector2000@protonmail.com>
2025-12-21 15:07:19 +0330

    alpha

--- Alpha Proj Renderer/Source.cpp
+++ Alpha Proj Renderer/Source.cpp
@@ -90,14 +90,14 @@ public:
     {
         int width = info.windowWidth;
         int height = info.windowHeight;
-      
+
         glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glUseProgram(shaderProgram);
-        glUniform2f(resLoc, (float)width, (float)height);
-        glUniform1f(timeLoc, (float)currentTime);
-      
+        glUniform2f(resLoc, (float)width, (float)height); 
+            glUniform1f(timeLoc, (float)currentTime);
+
 
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 6);
@@ -120,82 +120,3 @@ DECLARE_MAIN(mandelbrot_app);
 
 
 
-
-
-
-
-//ORIGINAL CMAKE MADE PROJECT CODE
-/* #include <sb7.h>
-#include <math.h>
-class simpleclear_app : public sb7::application
-{
-    void init()
-    {
-        static const char title[] = "Nigga";
-
-        sb7::application::init();
-
-        memcpy(info.title, title, sizeof(title));
-    }
-
-    virtual void render(double currentTime)
-    {
-        const GLfloat color[] = { (float)sin(currentTime) * 0.5f + 0.5f,
-        (float)cos(currentTime) * 0.5f + 0.5f,
-        0.0f, 1.0f };
-        glClearBufferfv(GL_COLOR, 0, color);
-    }
-};
-
-DECLARE_MAIN(simpleclear_app)
-
-
-
-
-GLuint compile_shaders(void)
-{
-    GLuint vertex_shader, fragment_shader, program;
-
-    static GLchar* vertex_shader_source[] =
-    {
-        "#version 450 core                                         \n"
-        "void main(void)                                              \n"
-        "{                                                                    \n"
-        "gl_Position= vec4(0.0,0.0,0.5,1.0);               \n"
-        "}                                                                   \n"
-    };
-
-    static GLchar* fragment_shader_source[] =
-    {
-        "#version 450 core                                      \n"
-        "out vec4 color;                                                \n"
-        "void main(void)                                              \n"
-        "{                                                                    \n"
-        "color= vec4(1.0,0.0,0.5,1.0);               \n"
-        "}                                                                   \n"
-    };
-//Create and compile vertex shader
-    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex_shader, 1, vertex_shader_source, NULL);
-    glCompileShader(vertex_shader);
-    //Create and compile fragment shader
-    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment_shader, 1, fragment_shader_source, 0);
-    glCompileShader(fragment_shader);
-    //Attach shader
-    program=glCreateProgram();
-    glAttachShader(program, vertex_shader);
-    glAttachShader(program, fragment_shader);
-
-    glLinkProgram(program);
-    glDeleteShader(vertex_shader);
-    glDeleteShader(fragment_shader);
-    return program;
-}
-
-
-
-
-
-
-*/
